09ba6f2d9b0854d3254e099bfbaee19574ebcf89
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-03-25 13:25:55 -0300

    Adicionando Cadastro, Login e Logout de usuário

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import { db, auth } from './firebaseConnection'
 
 import {
   doc,
-  setDoc,
   collection,
   addDoc,
-  getDoc,
   getDocs,
   updateDoc,
   deleteDoc,
@@ -30,6 +28,12 @@ function App() {
 
   const [posts, setPosts] = useState([])
 
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+
+  const [user, setUser] = useState(false)
+  const [userDetail, setUserDetail] = useState({})
+
   useEffect(() => {
     async function loadPosts() {
       const dados = onSnapshot(collection(db, "posts"), (snapshot) => {
@@ -50,6 +54,24 @@ function App() {
 
     loadPosts()
 
+  }, [])
+
+  useEffect(() => {
+    async function checkLogin() {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setUser(true)
+          setUserDetail({
+            uid: user.uid,
+            email: user.email
+          })
+        } else {
+          setUser(false)
+          setUserDetail({})
+        }
+      })
+    }
+    checkLogin()
   }, [])
 
   async function adicionarPost() {
@@ -122,9 +144,71 @@ function App() {
       })
   }
 
+  async function novoUsuario() {
+    await createUserWithEmailAndPassword(auth, email, senha)
+      .then(() => {
+        setEmail('')
+        setSenha('')
+      })
+      .catch((error) => {
+        if (error.code === 'auth/weak-password') {
+          alert("Senha muito fraca!")
+        } else if (error.code === 'auth/email-already-in-use') {
+          alert("Email já existe!")
+        }
+      })
+  }
+
+  async function logarUsuario() {
+    await signInWithEmailAndPassword(auth, email, senha)
+      .then(() => {
+        setUserDetail({
+          uid: value.user.id,
+          email: value.user.email
+        })
+        setUser(true)
+        setEmail('')
+        setSenha('')
+      })
+      .catch(() => {
+        alert("Erro ao fazer login!")
+      })
+  }
+
+  async function fazerLogout() {
+    await signOut(auth)
+    setUser(false)
+    setUserDetail({})
+  }
+
   return (
     <div>
       <h1>REACTJS + FIREBASE :^)</h1>
+      {
+        user && (
+          <div>
+            <strong>Seja bem-vindo(a) (Você está logado!)</strong><br />
+            <span>ID: {userDetail.uid} - Email: {userDetail.email}</span><br />
+            <button onClick={fazerLogout}>Sair da conta</button>
+          </div>
+        )
+      }
+
+      <div className='container'>
+        <h2>Usuários</h2>
+        <label>Email:</label>
+        <input placeholder='Digite o seu Email' value={email} onChange={(email) => setIdPost(email.target.value)} />
+        <br />
+        <label>Senha:</label>
+        <input placeholder='Digite o sua Senha' value={senha} onChange={(senha) => setIdPost(senha.target.value)} />
+        <br />
+        <button onClick={novoUsuario}>Cadastrar</button>
+        <button onClick={logarUsuario}> Fazer login</button>
+      </div>
+
+      <br />
+      <hr />
+
       <div className='container'>
         <h2>POSTS</h2>
 
@@ -138,7 +222,7 @@ function App() {
 
         <label>Autor:</label>
         <input placeholder='Digite o nome do Autor' type='text' value={autor} onChange={(autor) => setAutor(autor.target.value)} />
-        <br/>
+        <br />
 
         <button onClick={adicionarPost}>Cadastrar</button>
         <button onClick={buscaPost}>Buscar Post</button>
@@ -146,11 +230,11 @@ function App() {
 
         <ul>
           {posts.map((post) => {
-            return(
+            return (
               <li>
-                <span>ID: {post.id}</span><br/>
-                <span>Título: {post.titulo}</span><br/>
-                <span>Autor: {post.autor}</span><br/>
+                <span>ID: {post.id}</span><br />
+                <span>Título: {post.titulo}</span><br />
+                <span>Autor: {post.autor}</span><br />
                 <button onClick={() => excluirPost(post.id)}>Excluir</button>
               </li>
             )
